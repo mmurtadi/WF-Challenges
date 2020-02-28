e560e88dd39312f62079fc9b41bd153de0eac353
mmurtadi <mmurtadi@gmail.com>
2020-02-28 00:52:43 -0800

    added challenge 4 attempt

--- challenge3.js
+++ challenge3.js
@@ -2,11 +2,11 @@
 // Moe Murtadi
 // Feb 27, 2019
 
-var cardDeck = document.createElement("section");
+var cardDeck = document.createElement("div");
 cardDeck.setAttribute('class', 'card-deck');
+cardDeck.setAttribute('style', 'display: flex;margin: 60px auto 0;width: 80%;');
 cardDeck.setAttribute('id', 'card-deck-id');
-document.getElementById('top-pricing-section').appendChild(cardDeck);
-document.querySelector("#top-pricing-section > section.xs-pt4.sm-pt4.xs-pb3.sm-pb3.lg-py6")
+document.querySelector("#top-pricing-section > section.xs-pt4.sm-pt4.xs-pb3.sm-pb3.lg-py6").appendChild(cardDeck);
 
 var badges = ["Badge printing", "Legal document signing", "Visitor photos", "Multiple sign-in flows", "Visitor screening", "Delivery notifications"];
 var badgeImage = ["Badge printing", "Legal document signing", "Visitor photos", "Multiple sign-in flows", "Visitor screening", "Delivery notifications"];
@@ -14,11 +14,19 @@ var i;
 for (i = 0; i < badges.length; i++) {
     var card = document.createElement("div");
     var idName = "card" + i.toString();
-    card.setAttribute('class', 'card');
+    card.setAttribute('style', 'width: 15%;text-align: center;margin:10px auto;box-shadow: 3px 5px #555;border: 1px solid;padding: 10px;');
     card.setAttribute('id', idName);
+    //set Image
+    var nestedLogo =  document.createElement("img");
+    nestedLogo.setAttribute('src', 'https://pbs.twimg.com/profile_images/1158749853372936193/98lQXwYf_400x400.jpg');
+    nestedLogo.setAttribute('id', 'WFLogo');
+    nestedLogo.setAttribute('style', 'height: auto;width: 30px; margin:5px auto;display:block;');
+    
     document.getElementById('card-deck-id').appendChild(card);
     var cardText = document.createElement("p");
     cardText.innerHTML = badges[i];
+    
+    document.getElementById(idName).appendChild(nestedLogo);
     document.getElementById(idName).appendChild(cardText); 
 }
 
